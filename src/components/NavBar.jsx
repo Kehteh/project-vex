b3ffca10b447fe0d3c961e70a527b4022dd7b15f
Katie <katie.connell.91@gmail.com>
2024-11-30 12:19:47 +0800

    added redirect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ function NavBar() {
     <div>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/login">Log In</Link>
       </nav>
       {/* React Router will pass components into the <Outlet /> based on the path */}
       <Outlet />
