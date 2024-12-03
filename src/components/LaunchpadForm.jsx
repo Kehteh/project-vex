5740304ac0fafc03323c0f28af5197656c21fcea
Katie <katie.connell.91@gmail.com>
2024-12-03 20:51:23 +0800

    playing with pages

---
+++ src/components/LaunchpadForm.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import postLogin from "../api/post-login.js";
+import useAuth from "../hooks/use-auth.js";
+
+function CreateProject() {
+    return <h1>Project Launchpad</h1>;
+}
+
+export default LoginPage;
