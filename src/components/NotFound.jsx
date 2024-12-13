e4b68e1f1481e46fdcb5572bef96fa2ec8e27f50
Katie <katie.connell.91@gmail.com>
2024-12-13 22:28:25 +0800

    added a lottie

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,25 @@
 // NotFound.js
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from './lotties/Dino404';
 
-const NotFound = () => {
+export default function NotFound() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+    
     return (
-        <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>404</h1>
-            <p>Page Not Found</p>
-            <a href="/">Go to Home</a>
-        </div>
+      <div>
+        <Lottie 
+          options={defaultOptions}
+          height={400}
+          width={400}
+        />
+      </div>
     );
-};
-
-export default NotFound;
+}
